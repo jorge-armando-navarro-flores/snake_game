afac67d5baaceaf0f105c56849c937b9e3ac5788
jorge-armando-navarro-flores <jorge.armando.navarro.flores@gmail.com>
2021-08-23 12:51:58 -0500

    high_score

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
-score_board = ScoreBoard()
+scoreboard = ScoreBoard()
 
 
 screen.listen()
@@ -29,18 +29,19 @@ while game_is_on:
     if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
-        score_board.increase_score()
+        scoreboard.increase_score()
 
     # Detect collision with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        score_board.game_over()
+        scoreboard.reset()
+        snake.reset()
 
     # Detect collision with tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            score_board.game_over()
+            scoreboard.reset()
+            snake.reset()
+
 
 
 screen.exitonclick()
--- score_board.py
+++ score_board.py
@@ -6,6 +6,8 @@ FONT = ("Arial", 17, "normal")
 class ScoreBoard(Turtle):
     def __init__(self):
         self.score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         super().__init__()
         self.color("white")
         self.penup()
@@ -14,13 +16,23 @@ class ScoreBoard(Turtle):
         self.update_score()
 
     def update_score(self):
-        self.write(f"Score: {self.score}", False, align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", False, align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", False, align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", "w") as file:
+                file.write(f"self.high_score")
+        self.score = 0
+        self.update_score()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", False, align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_score()
+
+
